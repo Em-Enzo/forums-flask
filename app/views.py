7da6f72444a1f3a07a961dde612ce967ba5c9c58
Em-Enzo <amine1inzo@gmail.com>
2018-03-20 17:33:25 +0100

    Added CRUD controller actions

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for 
+from flask import Flask, render_template, request, redirect, url_for, abort 
 import models
 from app import app, member_store, post_store
 
@@ -18,3 +18,29 @@ def topic_add():
 		return redirect(url_for("home"))
 	else:
 		return render_template("topic_add.html")
+
+
+@app.route("/topic/delete/int:id>")
+def topic_delete(id):
+	post_store.delete(id)
+	return redirect(url_for("home"))
+
+
+@app.route("/topic/show/<int:id>")
+def topic_show(id):
+	post = post_store.get_by_id(id)
+	return render_template("topic_show.html", post= post)
+
+
+@app.route("/topic/edit/<int:id>", methods = ["GET", "POST"])
+def topic_update(id):
+	post = post_store.get_by_id(id)
+	if request.method == "POST":
+		post.title = request.form["title"]
+		post.content = request.form["content"]
+		post_store.update(post)
+		result redirect(url_for("home"))
+	elif request.method == "GET":
+		result = render_template("topic_update.html", post= post)
+
+	return result
